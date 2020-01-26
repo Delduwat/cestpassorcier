802df33ea9530d61df01d23e50a81e7ff59a0199
Deldu <deldu@antoines-macbook-air.home>
2020-01-26 15:31:36 +0100

    add requirement on README and modif on ping

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cestpassorcier
 c'est deux bots discord qui discutent
+
+# Requirement
+(aka gem install or bundler)
+discordrb
+json
+
--- src/ping.rb
+++ src/ping.rb
@@ -17,8 +17,23 @@ def get_token_from_config_file(file_name)
 	end
 end
 
-
-
+#attachments ⇒ Array<Attachment> readonly
+#author ⇒ Member, User (also: #user, #writer) readonly
+#channel ⇒ Channel readonly
+#content ⇒ String (also: #text, #to_s) readonly
+#edited ⇒ true, false (also: #edited?) readonly
+#edited_timestamp ⇒ Time (also: #edit_timestamp) readonly
+#embeds ⇒ Array<Embed> readonly
+#mention_everyone ⇒ true, false (also: #mention_everyone?, #mentions_everyone?) readonly
+#mentions ⇒ Array<User> readonly
+#nonce ⇒ String readonly
+#pinned ⇒ true, false (also: #pinned?) readonly
+#reactions ⇒ Hash<String => Reaction> readonly
+#role_mentions ⇒ Array<Role> readonly
+#timestamp ⇒ Time readonly
+#tts ⇒ true, false (also: #tts?) readonly
+#webhook_id ⇒ Integer? r
+list_of_element = ["attachments","author","channel","content","edited","edit_timestamp","embeds","mention_everyone","mentions","nonce","pinned","reactions","role_mentions","timestamp","tts","webhook_id"]
 ## main
 TOKEN = get_token_from_config_file("../discord.conf")
 begin
@@ -26,9 +41,11 @@ bot = Discordrb::Bot.new token: TOKEN
 rescue
 	say_error("discord bot object could not be created")
 else
-	bot.message(with_text: 'Ping!') do |event|
-	  event.respond 'Pong!'
-	  puts event
+	bot.message() do |event|
+	  #event.respond 'Pong!'
+	  puts ">>%s just said %s<<"%[event.author.username,event.content]
+	  #puts JSON.pretty_generate(event.to_json())
+
 	end
 	bot.run
 end
